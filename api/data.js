ea23cc49ca834cd4510095a5fdfae051f8288d06
Thiago Melchiors <thiago.franke.ms@gmail.com>
2026-03-02 19:31:18 -0300

    Update Edge Cache time

--- api/data.js
+++ api/data.js
@@ -335,7 +335,7 @@ request.query e.g.
 export default async function handler(request, response) {
   response.setHeader(
     "Cache-Control",
-    "public, s-maxage=86400, stale-while-revalidate=2073600"
+    "public, s-maxage=3600, stale-while-revalidate=86400"
   );
   const { widget } = request.query;
   try {
